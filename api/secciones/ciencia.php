2d118927dc7de2dcacea2951dea9b102b6f107d1
Zeladax <rodrigo.zp.00710@gmail.com>
2025-11-03 18:30:57 -0500

    noticia ciencia y tecnologia

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Científicos peruanos descubren nueva especie de rana en la Amazonía",
+"autor" => "Dr. Miguel Ángel Castro",
+"resumen" => "Un equipo de biólogos del Instituto de Investigaciones de la Amazonía Peruana identificó una nueva especie de rana venenosa en la región de Loreto. El anfibio, denominado Dendrobates amazonica, presenta características únicas de coloración y comportamiento reproductivo. El descubrimiento refuerza la importancia de proteger los ecosistemas amazónicos ante la creciente deforestación.",
 ];
 ?>
--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Inteligencia artificial revoluciona diagnóstico médico en hospitales peruanos",
+"autor" => "Patricia Ramírez Soto",
+"resumen" => "Cinco hospitales de Lima implementaron sistemas de IA para detectar enfermedades oncológicas con 95% de precisión. La tecnología, desarrollada en colaboración con universidades locales, reduce el tiempo de diagnóstico de semanas a minutos. El Ministerio de Salud planea expandir el programa a 20 centros hospitalarios en 2026, beneficiando a más de 2 millones de pacientes.",
 ];
 ?>
