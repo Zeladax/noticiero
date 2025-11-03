27cadac79368d95f1a38351137df25790a5bb0bf
Zeladax <rodrigo.zp.00710@gmail.com>
2025-11-03 18:38:34 -0500

    fila 2 q sigue a 3

--- api/secciones/git.php
+++ api/secciones/git.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Desarrolladores peruanos entre los más activos de Latinoamérica en GitHub",
+"autor" => "Ing. Marco Villanueva López",
+"resumen" => "Perú se ubicó en el tercer lugar de contribuciones a proyectos open source en GitHub durante 2025, según el informe anual de la plataforma. Los desarrolladores peruanos aportaron más de 500,000 commits a repositorios públicos, destacando en proyectos de inteligencia artificial y blockchain. La comunidad tech local se consolida como referente regional.",
 ];
 ?>
--- api/secciones/openstack.php
+++ api/secciones/openstack.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openstack = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Gobierno implementa infraestructura OpenStack para servicios digitales",
+"autor" => "Ing. Fernando Prado Suárez",
+"resumen" => "El Estado peruano implementó una plataforma OpenStack para hospedar servicios digitales gubernamentales, reduciendo dependencia de proveedores externos. La infraestructura de código abierto permite ahorros estimados de 5 millones de dólares anuales y garantiza soberanía digital. Más de 30 entidades públicas ya migraron sus aplicaciones a esta plataforma nacional.",
 ];
 ?>
