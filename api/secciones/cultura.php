d58e6937795186d63d17ce716f6a87d2df1925ac
Zeladax <rodrigo.zp.00710@gmail.com>
2025-11-03 18:32:31 -0500

    noticia cultura y nube

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Festival Internacional de Cine de Lima celebra 30 años con récord de asistencia",
+"autor" => "Carmen Silva Rodríguez",
+"resumen" => "El Festival de Cine de Lima cerró su edición aniversario con más de 80,000 espectadores, superando todas las expectativas. Se proyectaron 180 películas de 45 países, destacando el cine latinoamericano contemporáneo. La película peruana 'Retablo Andino' ganó el premio principal, consolidando el talento nacional en la escena cinematográfica internacional.",
 ];
 ?>
--- api/secciones/nube.php
+++ api/secciones/nube.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nube = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Empresas peruanas aceleran migración a la nube para optimizar operaciones",
+"autor" => "Ing. Luis Paredes Vega",
+"resumen" => "El 65% de empresas medianas y grandes en Perú han migrado al menos el 40% de sus operaciones a servicios cloud durante 2025. La adopción de AWS, Google Cloud y Azure ha permitido reducir costos operativos en 30% promedio y mejorar la escalabilidad. Expertos proyectan que para 2027 el 80% del sector empresarial operará principalmente en la nube.",
 ];
 ?>
